a1c71bcc5033c2c60f179556555339cba03284a2
Dheeraj Kumar <mthdheerajkumar@gmail.com>
2024-04-08 16:47:00 +0530

    text changes

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
 
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import Testimonial from "./Testimonial";
 import Service from "./Service";
 
@@ -8,7 +8,8 @@ const servicesData = [
   {
     icon: "/images/icon-design.svg",
     title: "Web design",
-    description: "The most modern and high-quality design made at a professional level."
+    description:
+      "The most modern and high-quality design made at a professional level."
   },
   {
     icon: "/images/icon-dev.svg",
@@ -23,54 +24,56 @@ const servicesData = [
   {
     icon: "/images/icon-photo.svg",
     title: "Photography",
-    description: "I make high-quality photos of any category at a professional level."
+    description:
+      "I make high-quality photos of any category at a professional level."
   }
 ];
 
-
 const About = () => {
-  const [testimonials , setTestimonials ] = useState([]);
-  useEffect(()=> {
-    fetch('testimonials.json').then(res => res.json()).then(data => {
-      console.log(data)
-      setTestimonials(data)
-    });
-  }, [])
+  const [testimonials, setTestimonials] = useState([]);
+  useEffect(() => {
+    fetch("testimonials.json")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setTestimonials(data);
+      });
+  }, []);
   return (
     <article className="about  active" data-page="about">
+      <header>
+        <h2 className="h2 article-title">About me</h2>
+      </header>
 
-    <header>
-      <h2 className="h2 article-title">About me</h2>
-    </header>
+      <section className="about-text">
+        <p>
+          Hello! I’m RAJ P DHEERAJ. Web Developer with over 3 years of
+          experience specializing in front end development. Experienced with all
+          stages of the development cycle for dynamic web projects.Having an
+          in-depth knowledge including advanced HTML5, CSS, CSS3, SASS, LESS,
+          JSON, XML, Java, JavaScript, JQuery, React JS. Strong background in
+          management and leadership.
+        </p>
 
-    <section className="about-text">
-      <p>
-        I'm Creative Director and UI/UX Designer from Sydney, Australia, working in web development and print media.
-        I enjoy
-        turning complex problems into simple, beautiful and intuitive designs.
-      </p>
+        <p>
+          My job is to build your website so that it is functional and
+          user-friendly but at the same time attractive. Moreover, I add
+          personal touch to your product and make sure that is eye-catching and
+          easy to use. My aim is to bring across your message and identity in
+          the most creative way. I created web design for many famous brand
+          companies.
+        </p>
+      </section>
 
-      <p>
-        My job is to build your website so that it is functional and user-friendly but at the same time attractive.
-        Moreover, I
-        add personal touch to your product and make sure that is eye-catching and easy to use. My aim is to bring
-        across your
-        message and identity in the most creative way. I created web design for many famous brand companies.
-      </p>
-    </section>
-
-
-    {/* <!--
+      {/* <!--
       - service
     --> */}
 
-    <section className="service">
+      <section className="service">
+        <h3 className="h3 service-title">What i'm doing</h3>
 
-      <h3 className="h3 service-title">What i'm doing</h3>
-
-      <ul className="service-list">
-
-      {servicesData.map((service, index) => (
+        <ul className="service-list">
+          {servicesData.map((service, index) => (
             <Service
               key={index}
               icon={service.icon}
@@ -78,23 +81,18 @@ const About = () => {
               description={service.description}
             />
           ))}
+        </ul>
+      </section>
 
-      </ul>
-
-    </section>
-
-
-    {/* <!--
+      {/* <!--
       - testimonials
     --> */}
 
-    <section className="testimonials">
+      <section className="testimonials">
+        <h3 className="h3 testimonials-title">Testimonials</h3>
 
-      <h3 className="h3 testimonials-title">Testimonials</h3>
-
-      <ul className="testimonials-list has-scrollbar">
-
-      {testimonials.map((testimonial, index) => (
+        <ul className="testimonials-list has-scrollbar">
+          {testimonials.map((testimonial, index) => (
             <Testimonial
               key={index}
               name={testimonial.name}
@@ -102,64 +100,56 @@ const About = () => {
               testimonial={testimonial.testimonial}
             />
           ))}
+        </ul>
+      </section>
 
-      </ul>
-
-    </section>
-
-
-    {/* <!--
+      {/* <!--
       - clients
     --> */}
 
-    <section className="clients">
+      <section className="clients">
+        <h3 className="h3 clients-title">Clients</h3>
 
-      <h3 className="h3 clients-title">Clients</h3>
+        <ul className="clients-list has-scrollbar">
+          <li className="clients-item">
+            <a href="#">
+              <img src="images/logo-1-color.png" alt="client logo" />
+            </a>
+          </li>
 
-      <ul className="clients-list has-scrollbar">
+          <li className="clients-item">
+            <a href="#">
+              <img src="images/logo-2-color.png" alt="client logo" />
+            </a>
+          </li>
 
-        <li className="clients-item">
-          <a href="#">
-            <img src="images/logo-1-color.png" alt="client logo"/>
-          </a>
-        </li>
+          <li className="clients-item">
+            <a href="#">
+              <img src="images/logo-3-color.png" alt="client logo" />
+            </a>
+          </li>
 
-        <li className="clients-item">
-          <a href="#">
-            <img src="images/logo-2-color.png" alt="client logo"/>
-          </a>
-        </li>
+          <li className="clients-item">
+            <a href="#">
+              <img src="images/logo-4-color.png" alt="client logo" />
+            </a>
+          </li>
 
-        <li className="clients-item">
-          <a href="#">
-            <img src="images/logo-3-color.png" alt="client logo"/>
-          </a>
-        </li>
+          <li className="clients-item">
+            <a href="#">
+              <img src="images/logo-5-color.png" alt="client logo" />
+            </a>
+          </li>
 
-        <li className="clients-item">
-          <a href="#">
-            <img src="images/logo-4-color.png" alt="client logo"/>
-          </a>
-        </li>
+          <li className="clients-item">
+            <a href="#">
+              <img src="images/logo-6-color.png" alt="client logo" />
+            </a>
+          </li>
+        </ul>
+      </section>
+    </article>
+  );
+};
 
-        <li className="clients-item">
-          <a href="#">
-            <img src="images/logo-5-color.png" alt="client logo"/>
-          </a>
-        </li>
-
-        <li className="clients-item">
-          <a href="#">
-            <img src="images/logo-6-color.png"alt="client logo"/>
-          </a>
-        </li>
-
-      </ul>
-
-    </section>
-
-  </article>
-  )
-}
-
-export default About
+export default About;

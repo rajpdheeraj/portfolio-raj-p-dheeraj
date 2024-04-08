e6001c5eb5cb48f248860165253ff4c8d02ab297
Dheeraj Kumar <mthdheerajkumar@gmail.com>
2024-04-08 16:30:09 +0530

    first commit

---
+++ src/pages/Contact/Contact.jsx
@@ -0,0 +1,46 @@
+
+
+const Contact = () => {
+  return (
+    <section className="contact" data-page="contact">
+
+      <header>
+        <h2 className="h2 article-title">Contact</h2>
+      </header>
+
+      <section className="mapbox" data-mapbox>
+        <figure>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3564.756144201255!2d85.1650727752162!3d26.688280476781667!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ecc789669d76ff%3A0x1f4ea77132090b1d!2sRAJ%20P%20DHEERAJ!5e0!3m2!1sen!2sin!4v1712573625262!5m2!1sen!2sin"
+            width="400" height="300" loading="lazy" title="Google Map"></iframe>
+        </figure>
+      </section>
+
+      <section className="contact-form">
+
+        <h3 className="h3 form-title">Contact Form</h3>
+
+        <form action="#" className="form" data-form>
+
+          <div className="input-wrapper">
+            <input type="text" name="fullname" className="form-input" placeholder="Full name" required data-form-input />
+
+            <input type="email" name="email" className="form-input" placeholder="Email address" required data-form-input />
+          </div>
+
+          <textarea name="message" className="form-input" placeholder="Your Message" required data-form-input></textarea>
+
+          <button className="form-btn" type="submit" data-form-btn>
+            <ion-icon name="paper-plane"></ion-icon>
+            <span>Send Message</span>
+          </button>
+
+        </form>
+
+      </section>
+
+    </section>
+  );
+};
+
+export default Contact;
